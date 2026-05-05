78df8bfc8f33f8f4876a20dd6c77d6cb63bce713
devsmish <smyshynskyi@gmail.com>
2026-05-05 15:42:27 +0200

    create variable for views.py

--- source_manager/views.py
+++ source_manager/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 
 
-def greetings(request: HttpRequest): -> HttpResponse
-    name = 'Sergio'
-    return HttpResponse(f"Hello, {name}!")
+def greetings(request: HttpRequest) -> HttpResponse:
+    username = 'Sergio'
+    return HttpResponse(f"Hello, {username}!")
