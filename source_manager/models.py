e3b3c682959e5dfb55c029ec95b03ad3a000866c
devsmish <smyshynskyi@gmail.com>
2026-05-06 19:54:01 +0200

    create 2 migrations and update models.py and admin.py in app source_manager

--- source_manager/models.py
+++ source_manager/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)  # VarChar(255)
+    description = models.TextField()
+    price = models.FloatField()
+    published_date = models.DateField()
+
+# Миграции и управление моделями отвечают ИСКЛЮЧИТЕЛЬНО ЗА DDL категорию запросов
+
+# DDL query -> Data Definition Language
+# DML query -> Data Manipulation Language
+# """
+# CREATE TABLE IF NOT EXISTS 'test_app_book' (
+#     id ...
+#     title VarChar(100) NOT NULL
+#     description TEXT NOT NULL
+#     published_date DATE
+# )
+# """
